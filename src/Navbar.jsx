1fdc93c7c576073043ec3f06c3e2c12e0b3d4f03
Kenny Hector <kennyhector24@gmail.com>
2025-06-10 16:19:30 -0400

    Update styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,12 +5,14 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-left">
+        <span className="site-name">Kenny Hector</span>
+      </div>
+      <div className="nav-right">
         <Link to="/">Home</Link>
         <Link to="/portfolio">Portfolio</Link>
         <Link to="/testimonials">Testimonials</Link>
         <Link to="/contact">Contact</Link>
       </div>
-      <div className="nav-right">Kenny Hector</div>
     </nav>
   );
 }
